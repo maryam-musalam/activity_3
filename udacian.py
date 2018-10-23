2c6132caddb6c8646f9c25f9847c4842a407206f
maryam <maryam-musalam@hotmail.com>
2018-10-23 21:28:50 +0300

    define the __init__ function

--- udacian.py
+++ udacian.py
@@ -1,2 +1,8 @@
 class Udacian():
+      def __init__(self , name , city , entrollment , nanodegree, ststus):
+          self.udacian_name= name
+          self.udacian_city= city
+          self.udacian_entrollment = entrollment
+          self.udacian_nanodegree= nanodegree
+          self.udacian_ststus = ststus
 
